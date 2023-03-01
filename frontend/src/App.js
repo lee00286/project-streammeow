24c2ad3feabb58f39708d47dbb9ee70dc0e3f592
lee00286 <33945159+lee00286@users.noreply.github.com>
2023-02-28 21:08:22 -0500

    feat: add client side routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+// Components
+import HomePage from "./Components/HomePage/HomePage";
+// Style
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* EXAMPLE: <Route path="/" element={<COMPONENT-NAME />} /> */}
+      </Routes>
     </div>
   );
 }
