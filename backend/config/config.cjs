d9c885f6083c37dfd4300c2e3b2fb260a6b5258a
lee00286 <33945159+lee00286@users.noreply.github.com>
2023-03-13 21:27:50 -0400

    fix: typo on env variable

--- backend/config/config.cjs
+++ backend/config/config.cjs
@@ -3,21 +3,21 @@ require("dotenv").config();
 module.exports = {
   development: {
     username: process.env.POSTGRES_USERNAME || "root",
-    password: process.env.POSTGRES_PASSWORd || null,
+    password: process.env.POSTGRES_PASSWORD || null,
     database: process.env.POSTGRES_DB || "streammeow",
     host: process.env.POSTGRES_HOST || "localhost",
     dialect: "postgres",
   },
   test: {
     username: process.env.POSTGRES_USERNAME || "root",
-    password: process.env.POSTGRES_PASSWORd || null,
+    password: process.env.POSTGRES_PASSWORD || null,
     database: process.env.POSTGRES_DB || "streammeow",
     host: process.env.POSTGRES_HOST || "localhost",
     dialect: "postgres",
   },
   production: {
     username: process.env.POSTGRES_USERNAME || "root",
-    password: process.env.POSTGRES_PASSWORd || null,
+    password: process.env.POSTGRES_PASSWORD || null,
     database: process.env.POSTGRES_DB || "streammeow",
     host: process.env.POSTGRES_HOST || "localhost",
     dialect: "postgres",
